6f17d6aa201c4a8961bc893764f0e03311413573
unknown <technicalsalma@gmail.com>
2022-09-07 22:19:51 +0600

    home page cleare

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Featured from '../../components/featured/Featured';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
 
 import "./home.css"
 
@@ -13,12 +15,12 @@ const Home = () => {
            <Header/>
            <div className="homeContainer">
            <Featured/>
-        {/* <h1 className="homeTitle">Browse by property type</h1>
+        <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties/>
-        <MailList/>
-        <Footer/> */}
+        {/* <MailList/> */}
+        {/* <Footer/> */}
       </div>
         </div>
     );
