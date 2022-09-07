2e0be39ffa809bfb2cff09dba34009999cdb10f2
unknown <technicalsalma@gmail.com>
2022-09-07 22:34:04 +0600

    mailList add

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import Featured from '../../components/featured/Featured';
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 import Header from '../../components/header/Header';
+import MailList from '../../components/mailList/MailList';
 import Navbar from '../../components/navbar/Navbar';
 import PropertyList from '../../components/propertyList/PropertyList';
 
@@ -19,7 +20,7 @@ const Home = () => {
         <PropertyList/>
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties/>
-        {/* <MailList/> */}
+        <MailList/>
         {/* <Footer/> */}
       </div>
         </div>
